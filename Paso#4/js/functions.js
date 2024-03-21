616b0c1df97d07efb75e00fbfd797a484a63c371
Franklin <fcastror@ucenfotec.ac.cr>
2024-03-20 21:43:41 -0600

    Updates

--- Paso#4/js/functions.js
+++ Paso#4/js/functions.js
@@ -1,27 +1,205 @@
+let changeLanguage = {
+    "spanish":{
+        "navbar":{
+            "home":"Inicio",
+            "profile":"Perfil",
+            "skills":"Conocimientos",
+            "projects":"Proyectos",
+            "works":"Trabajos",
+            "contact":"Contacto"
+        },
+    
+        "home":{
+            "card-title":"Desarrollador Full Stack",
+            "card-button":"Descargar CV",
+            "about-title":"¡Hola mundo!",
+            "about-sub-title-01":"Soy Franklin",
+            "about-sub-title-02":"Desarrollador Full Stack",
+            "about-description":"Ayudo a que las empresas crezcan mediante la creación de increíbles soluciones de software. Si buscas un desarrollador orientado a generar soluciones de calidad.",
+            "about-button":"Contáctame"
+        },
+    
+        "profile":{
+            "profile-title":"Sobre mí",
+            "profile-description-01":"Mi nombre es Franklin Castro, soy desarrollador de software, apasionado por el arte de hacer código.",
+            "profile-description-02":"Me especializo en tecnologías como: HTML, CSS, Javascript y Bootstrap. Estas habilidades me permiten crear interfaces de usuario visualmente atractivas y responsivas que mejoran la experiencia general del usuario.",
+            "profile-description-03":"Estoy orientado a generar soluciones a través de la comunicación, la innovación y la invención tecnológica. Comprometido con el desarrollo humano a través del progreso tecnológico ético."
+        },
+    
+        "skills":{
+            "skills-title":"CONOCIMIENTOS",
+            "skills-sub-title":"Soy un apasionado por el conocimiento y aprendizaje continuo",
+            "skills-stack-01":"Front-end",
+            "skills-stack-02":"Back-end",
+            "skills-stack-03":"Bases de datos",
+            "skills-stack-04":"Otros"
+        },    
+        
+        "projects":{
+            "projects-title":"PROYECTOS",
+            "projects-sub-title":"Estos son algunos de los proyectos en los que he trabajado",
+    
+            "project-01-title":"Cenfotec Kingdoms",
+            "project-01-sub-title-01":"2023",
+            "project-01-sub-title-02":"Videojuego de estrategia",
+            "project-01-sub-title-03":"Tecnologías",
+            "project-01-description":"Videojuego desarrollado utilizando Java como parte del Curso de Patrones de Programación de la Universidad Cenfotec.",
+            "project-01-big-description-01":"El videojuego basado en Java, desarrollado como parte del Curso de Patrones de Programación de la Universidad Cenfotec, es un testimonio de su sólido diseño e implementación. Al demostrar competencia en varios patrones de programación, como Singleton, Factory, Observer y Strategy, entre otros, el juego muestra un dominio de la arquitectura de sistemas complejos dentro de Java.",
+            "project-01-big-description-02":"El juego sirve como un ejemplo práctico de desarrollo de software, destacando la aplicación de programación orientada a objetos y patrones de diseño en un proyecto del mundo real. El éxito de este proyecto indica una sólida comprensión de los principios y mejores prácticas del diseño de software.",
+    
+            "project-02-title":"MobyDyg",
+            "project-02-sub-title-01":"2022",
+            "project-02-sub-title-02":"Comercio Electrónico",
+            "project-02-sub-title-03":"Tecnologías",
+            "project-02-description":"Plataforma de comercio diseñada con tecnologías de vanguardia, incluidas MySQL, AWS, Angular, TypeScript y NodeJS.",
+            "project-02-big-description-01":"La plataforma E-Commerce es una solución integral de compras online que integra varias tecnologías de vanguardia. Utiliza MySQL para una gestión de datos eficiente, AWS para una infraestructura de nube confiable y escalable y Angular con TypeScript para una interfaz de usuario dinámica y receptiva.",
+            "project-02-big-description-02":"El backend de la plataforma de comercio electrónico aprovecha AWS Lambda con NodeJS, mejorando su escalabilidad y reduciendo los gastos operativos. AWS API Gateway complementa esta configuración, proporcionando una capa API segura y administrada para una comunicación fluida entre los componentes frontend y backend. Juntas, estas tecnologías garantizan un manejo eficiente y rentable de los procesos backend, contribuyendo al rendimiento y la confiabilidad generales de la plataforma.",
+    
+            "project-03-title":"3Pillar Global Costa Rica",
+            "project-03-sub-title-01":"2021",
+            "project-03-sub-title-02":"Plataforma de Torneos",
+            "project-03-sub-title-03":"Tecnologías",
+            "project-03-description":"Plataforma web de la Federación Costarricense de Taekwondo para la gestión de torneos, construida con tecnologías como: React, Javascript, C#, ASP.NET MVC y Microsoft SQL Server.",
+            "project-03-big-description-01":"La plataforma web desarrollada para la Federación Costarricense de Taekwondo para la gestión de torneos es una solución integral construida con una moderna tecnología. Utilizando React para el frontend, JavaScript para el comportamiento dinámico, C# para la funcionalidad backend con ASP.NET MVC y Microsoft SQL Server para la gestión de bases de datos, la plataforma ofrece un sistema sólido y eficiente para gestionar torneos.",
+            "project-03-big-description-02":"Esta plataforma agiliza los procesos de organización de torneos, brindando a los administradores herramientas para la programación, la gestión de participantes y el seguimiento de resultados. Su uso de tecnologías contemporáneas garantiza una experiencia receptiva y fácil de usar para todas las partes interesadas involucradas en los torneos de taekwondo."
+        },
+    
+        "works":{
+            "works-title":"TRABAJOS",
+            "works-sub-title":"Si desea saber más sobre mi perfil profesional, pongámonos en contacto usando los siguientes espacios web.",
+            "linkedin":"Descubre más sobre mí en LinkedIn",
+            "github":"Encuentra mis proyectos en GitHub",
+            "whatsapp":"Hablemos sobre tu proyecto por WhatsApp"
+        },
+    
+        "contact":{
+            "contact-title":"CONTACTO",
+            "contact-sub-title":"Siempre estoy atento a nuevas oportunidades",
+            "contact-description":"Hablemos de su proyecto y transformemos las ideas en un negocio real.",
+            "form-name":"Ingresa tu nombre",
+            "form-email":"Ingresa tu correo",
+            "form-message":"Cuéntame sobre tu proyecto",
+            "form-button":"Enviar"
+        },
+    
+        "footer":{
+            "footer-title":"Desarrollador Full Stack - Franklin Castro"
+        }
+    },
+
+    "english":{
+        "navbar":{
+            "home":"Home",
+            "profile":"Profile",
+            "skills":"Skills",
+            "projects":"Projects",
+            "works":"Works",
+            "contact":"Contact"
+        },
+    
+        "home":{
+            "card-title":"Full-stack software developer",
+            "card-button":"Dowload CV",
+            "about-title":"Hello World!",
+            "about-sub-title-01":"I'm Franklin",
+            "about-sub-title-02":"Full-Stack Software Developer",
+            "about-description":"I help business grow by crafting amazing software solutions. If you're looking for a developer that likes to get stuff done.",
+            "about-button":"Let's talk!"
+            
+        },
+    
+        "profile":{
+            "profile-title":"About me",
+            "profile-description-01":"My name is Franklin, I'm a software developer, passionate about the art of code.",
+            "profile-description-02":"I am proficient in HTML, CSS, Javascript, and Bootstrap. These skills allow me to create visually appealing and responsive user interfaces that enhance the overall user experience.",
+            "profile-description-03":"I'm oriented towards generating solutions through communication, innovation, and technological invention. Committed to human development through ethical technological progress."
+        },
+    
+        "skills":{
+            "skills-title":"SKILLS",
+            "skills-sub-title":"I am striving to never stop learning and improving",
+            "skills-stack-01":"Front-end",
+            "skills-stack-02":"Back-end",
+            "skills-stack-03":"Database",
+            "skills-stack-04":"Others"
+        },    
+        
+        "projects":{
+            "projects-title":"PROJECTS",
+            "projects-sub-title":"I had the pleasure of working on these awesome projects",
+    
+            "project-01-title":"Cenfotec Kingdoms",
+            "project-01-sub-title-01":"2023",
+            "project-01-sub-title-02":"Strategy Game Project",
+            "project-01-sub-title-03":"Technologies",
+            "project-01-description":"Video game developed using Java as part of the Programming Patterns Course at Cenfotec University.",
+            "project-01-big-description-01":"The Java-based video game, developed as part of the Programming Patterns Course at Cenfotec University, is a testament to its robust design and implementation. Demonstrating a proficiency in various programming patterns such as Singleton, Factory, Observer, and Strategy, among others, the game showcases a mastery of complex system architecture within Java.",
+            "project-01-big-description-02":"The game serves as a practical example of software development, highlighting the application of object-oriented programming and design patterns in a real-world project. This project's success indicates a solid understanding of software design principles and best practices.",
+    
+            "project-02-title":"MobyDyg",
+            "project-02-sub-title-01":"2022",
+            "project-02-sub-title-02":"E-Commerce",
+            "project-02-sub-title-03":"Technologies",
+            "project-02-description":"Commerce platform crafted with cutting-edge technologies, including MySQL, AWS, Angular, TypeScript, and NodeJS.",
+            "project-02-big-description-01":"The E-Commerce platform is a comprehensive online shopping solution that integrates several cutting-edge technologies. It utilizes MySQL for efficient data management, AWS for reliable and scalable cloud infrastructure, and Angular with TypeScript for a responsive and dynamic user interface.",
+            "project-02-big-description-02":"The E-Commerce platform's backend leverages AWS Lambda with NodeJS, enhancing its scalability and reducing operational overhead. AWS API Gateway complements this setup, providing a secure and managed API layer for seamless communication between the frontend and backend components. Together, these technologies ensure efficient and cost-effective handling of backend processes, contributing to the platform's overall performance and reliability.",
+    
+            "project-03-title":"3Pillar Global Costa Rica",
+            "project-03-sub-title-01":"2021",
+            "project-03-sub-title-02":"Tournament Platform",
+            "project-03-sub-title-03":"Technologies",
+            "project-03-description":"Web platform of the Costa Rican Taekwondo Federation for tournament management, built with technologies such as: React, Javascript, C#, ASP.NET MVC and Microsoft SQL Server.",
+            "project-03-big-description-01":"The web platform developed for the Costa Rican Taekwondo Federation for tournament management is a comprehensive solution built with a modern technology stack. Utilizing React for the frontend, JavaScript for dynamic behavior, C# for backend functionality with ASP.NET MVC, and Microsoft SQL Server for database management, the platform offers a robust and efficient system for managing tournaments.",
+            "project-03-big-description-02":"This platform streamlines tournament organization processes, providing administrators with tools for scheduling, participant management, and result tracking. Its use of contemporary technologies ensures a responsive and user-friendly experience for all stakeholders involved in taekwondo tournaments."
+        },
+    
+        "works":{
+            "works-title":"WORKS",
+            "works-sub-title":"If you want to know more about my professional profile, let's get in touch using the following web spaces.",
+            "linkedin":"Connect with me on LinkedIn",
+            "github":"Find me on GitHub",
+            "whatsapp":"Let's talk about your project"
+        },
+    
+        "contact":{
+            "contact-title":"CONTACT",
+            "contact-sub-title":"I'm always open to new opportunities",
+            "contact-description":"Let's talk about your project and transform ideas into a real business.",
+            "form-name":"Enter your name",
+            "form-email":"Enter your email",
+            "form-message":"Enter your message",
+            "form-button":"Send Message"
+        },
+    
+        "footer":{
+            "footer-title":"Full-stack Software Developer - Franklin Castro"
+        }
+    }
+};
+
+
 /**
  * The function `changeLanguage` asynchronously fetches language JSON files based on a switch value and
  * updates text content on the webpage accordingly.
  */
-const changeLanguage = async () => {
-   
+const changeLanguageFunction = async () => {
     const switchValue = document.getElementById("flexSwitchCheckLanguage").checked;
-    
     let requestJson;
-
+    
     if (switchValue) {
-        requestJson = await fetch(`./languages/es.json`);
+        requestJson = changeLanguage["spanish"];
     } else {
-        requestJson = await fetch(`./languages/en.json`);
+        requestJson = changeLanguage["english"];
     }
-
-    const texts = await requestJson.json();
+    
+    const texts = requestJson;
     const textsToChange = document.querySelectorAll("[data-section]");
     
-    for(const textToChange of textsToChange) {
+    textsToChange.forEach(textToChange => {
         const section = textToChange.dataset.section;
         const value = textToChange.dataset.value;
         textToChange.innerHTML = texts[section][value];
-    }
+    });
 }
 
 
@@ -72,6 +250,6 @@ const contactEmail = () => {
 //**** Event Listeners ****//
 //*************************//
 
-document.getElementById("flexSwitchCheckLanguage").onclick = changeLanguage;
+document.getElementById("flexSwitchCheckLanguage").onclick = changeLanguageFunction;
 
 document.getElementById("sendMessageButton").onclick = contactEmail;
